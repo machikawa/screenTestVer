f8ebbeeae2a1d173a833b2cbe7d0f3bb95db2f90
Townriver <a1938hm@aiit.ac.jp>
2019-08-24 12:51:14 +0900

    Refactor -2 finishied

--- animationTest/Constants.swift
+++ animationTest/Constants.swift
@@ -10,8 +10,6 @@ import Foundation
 import UIKit
 
 class Constants {
-    // 画像集 // Refuctor1 8/24-1 画像を配列に
-    let imageArray2 = [UIImage(named: "d001"), UIImage(named: "d002"), UIImage(named: "beer.jpg")]
     // ボタンのイメージ
     let startBtn2 = UIImage(named: "saisei.png")
     let stopBtn2 = UIImage(named: "stop.jpg")
@@ -20,6 +18,6 @@ class Constants {
     let activeBtnColor2 = UIColor.blue
     let inactiveBtnColor2 = UIColor.gray
     
-    // タイマー値 // Refuctor 2 8/24-1 インターバルを double に
-    var interval:Double = 0.5
+    // タイマー値 
+    var interval2:Double = 2
 }
--- animationTest/ViewController.swift
+++ animationTest/ViewController.swift
@@ -16,20 +16,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var prevBtn: UIButton!
     // 画像集 // Refuctor1 8/24-1 画像を配列に
     let imageArray = [UIImage(named: "d001"), UIImage(named: "d002"), UIImage(named: "beer.jpg")]
-    // ボタンのイメージ
-    let startBtn = UIImage(named: "saisei.png")
-    let stopBtn = UIImage(named: "stop.jpg")
-    
-    // ボタンの色
-    let activeBtnColor = UIColor.blue
-    let inactiveBtnColor = UIColor.gray
-
-    // タイマー値 // Refuctor 2 8/24-1 インターバルを double に
-    var interval:Double = 0.5
-
     // 画像インデクス
     var imageIndicator:Int = 0
-    
     // デフォルトの再生状態。タイマーのインスタンス化
     var isPlay:Bool = false
     var timer:Timer!
@@ -42,42 +30,31 @@ class ViewController: UIViewController {
         screenPictureSetter(self.imageIndicator) // スライドの絵の設定
 
         // 再生ボタンの初期処理, 初期は再生状況はオフ。
-        saiseiBtn.setImage(startBtn, for: .normal) // 中央の再生ボタンのイメージの設定
+        saiseiBtn.setImage(Constants.init().startBtn2, for: .normal) // 中央の再生ボタンのイメージの設定
         self.isPlay = false          // 画面ロード時に再生ボタンの再生状況を初期化
     }
-    
-    // スライドの画像をセットする // Refuctor1 8/24-1 配列を使ったらとてもがぞう変更が楽になった
-    func screenPictureSetter(_ indicator: Int) {
-        screenPicture.image = imageArray[indicator % imageArray.count]
-    }
-    
-    // QQ疑問！！
-    // sender の Any と UIButton の違いと、そもそも Sender の定義が今ひとつようわからん。 宣言の前のアンダーバーも。。
-    
+////////////////// ACTIONS //////////////////
+    // QQ疑問！！  sender の Any と UIButton の違いと、そもそも Sender の定義が今ひとつようわからん。 宣言の前のアンダーバーも。。
     // ボタン押下のアクション。
     @IBAction func nextBtn(_ sender: Any) {
         changePicture(1) // 一つすすむ
     }
-
     @IBAction func prevBtn(_ sender: Any) {
         changePicture(-1) // 一つもどる
     }
-    
     @IBAction func saiseiBtn(_ sender: Any) {
-        // 再生-> 停止
         if self.isPlay  {
-            pushSaiseiButton(startBtn, false, activeBtnColor)
-            // 自動再生をやめる
-            self.timer.invalidate()
-            self.timer = nil
-            // 停止ー＞再生
+            pushSaiseiButton(Constants.init().startBtn2, false, Constants.init().activeBtnColor2)
         } else if !self.isPlay {
-            pushSaiseiButton(stopBtn, true, inactiveBtnColor)
-            // 自動再生を開始する
-            self.timer = Timer.scheduledTimer(timeInterval: TimeInterval(interval), target: self, selector: #selector(timerUpdate(_:)), userInfo: nil, repeats: true)
+            pushSaiseiButton(Constants.init().stopBtn2, true, Constants.init().inactiveBtnColor2)
         }
-
     }
+////////////////// FUNCTIONS //////////////////
+    // スライドの画像をセットする // Refuctor1 8/24-1 配列を使ったらとてもがぞう変更が楽になった
+    func screenPictureSetter(_ indicator: Int) {
+        screenPicture.image = imageArray[indicator % imageArray.count]
+    }
+
     // 次へ戻るボタン押下時の詳細な処理。一応、進む数を１以外にすることも考えてIntで受ける。Boolでもよかったのだが。
     // Refuctor1 8/24-1 imageIndicator の受け方とかさん方法がや誤っていた。
     func changePicture(_ changeIndex: Int) {
@@ -93,6 +70,7 @@ class ViewController: UIViewController {
     }
 
     // 再生ボタン押下時の処理 // Refuctor1 8/24-1 引数を減らした。なぜか changePlaystete が let 扱いでトグルが使えない。
+    // Refactor 2 8/24-2 タイマー活動もこちらに記述
     func pushSaiseiButton(_ buttonimage : UIImage!,_ changedPlayState: Bool, _ buttonColor: UIColor){
         self.saiseiBtn.setImage(buttonimage, for: .normal)
         self.isPlay = changedPlayState
@@ -100,10 +78,19 @@ class ViewController: UIViewController {
         self.prevBtn.setTitleColor(buttonColor, for: .normal)
         self.nextBtn.isEnabled = !changedPlayState
         self.prevBtn.isEnabled = !changedPlayState
+        
+        if isPlay {
+            // 自動再生を開始する
+            self.timer = Timer.scheduledTimer(timeInterval: TimeInterval(Constants.init().interval2), target: self, selector: #selector(timerUpdate(_:)), userInfo: nil, repeats: true)
+        } else {
+            // 自動再生をやめる
+            self.timer.invalidate()
+            self.timer = nil
+        }
     }
-
+    
+    // タイマーのアップデート処理
     @objc func timerUpdate(_ timer: Timer) {
         changePicture(1)
     }
-
 }
